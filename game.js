781a3cc735e8fe6474c7a46914781e1ec34dbd00
Jordan Wilhite <jordanwilhite@gmail.com>
2015-08-14 06:51:30 -0500

    setting up the js file

---
+++ game.js
@@ -0,0 +1,16 @@
+;(function(){
+  var Game = function(canvasId){
+    var canvas = document.getElementById(canvasId);
+    var screen = canvas.getContext('2d');
+    var gameSize = { };
+
+  };
+
+  Game.prototype ={
+
+  };
+
+  window.onload = function() {
+    new Game("screen");
+  };
+})();
