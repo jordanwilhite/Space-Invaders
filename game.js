1bdc0bc1b410412fabee368cfa8e1bacfcb5b1e9
Jordan Wilhite <jordanwilhite@gmail.com>
2015-08-14 07:02:16 -0500

    added game screen

--- game.js
+++ game.js
@@ -2,11 +2,28 @@
   var Game = function(canvasId){
     var canvas = document.getElementById(canvasId);
     var screen = canvas.getContext('2d');
-    var gameSize = { };
+    var gameSize = {x: canvas.width, y: canvas.height };
 
+    this.bodies = [new Player (this, gameSize)];
+
+    var self = this;
+    var tick = function() {
+      self.update();
+      self.draw(screen, gameSize);
+      requestAnimationFrame(tick);
+    };
+    tick();
   };
 
   Game.prototype ={
+    update: function() {
+    },
+    draw: function(screen, gameSize){
+      screen.fillRect(30, 30, 40, 40);
+    }
+  };
+
+  var Player = function (){
 
   };
 
